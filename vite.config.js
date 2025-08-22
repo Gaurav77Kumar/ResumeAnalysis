be67ce880385d86dcb663d2bcb24f1b69bc905ac
Gaurav <gauravkumar732006@gmail.com>
2025-08-22 15:09:04 +0530

    inital commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite"; // 👈 Tailwind Vite plugin
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+
+    plugins: [react(), reactRouter(), tailwindcss()],
+});
